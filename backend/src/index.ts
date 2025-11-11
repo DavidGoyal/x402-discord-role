5c47fe2433d7d07134f9a4c5de3aeca542edf634
DavidGoyal <goyaldavid55@gmail.com>
2025-11-11 21:57:37 +0530

    pushing to vercel

---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import { authenticate } from "../middleware";
+import accessRoutes from "../routes/access";
+import serverRoutes from "../routes/server";
+import userRoutes from "../routes/user";
+import cors from "cors";
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Health check
+app.get("/health", (req, res) => {
+  res.json({ status: "OK", timestamp: new Date().toISOString() });
+});
+
+app.use("/api/user", accessRoutes);
+app.use(authenticate);
+// Routes
+app.use("/api", userRoutes);
+app.use("/api", serverRoutes);
+
+export default app;
